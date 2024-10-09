8b51b945ece4a733c4cca2607d029e6f568ee796
shahriartamim2 <202011039@ye.butex.edu.bd>
2024-10-09 12:27:29 +0600

    edgecanny and otsu threshold then converted into binary to detect the fault

--- skimage/entropyThreshold/main.py
+++ skimage/entropyThreshold/main.py
@@ -4,9 +4,16 @@ from skimage import io, restoration
 from skimage.filters.rank import entropy
 from skimage.morphology import disk 
 from skimage.filters import try_all_threshold
+from skimage import feature
+from skimage.filters import threshold_otsu
 
-img = io.imread("../images/defect.jpeg", as_gray=True)
-entropyImg = entropy(img, disk(3))
+img = io.imread("../images/weftD.jpg", as_gray=True)
+#entropyImg = entropy(img, disk(3))
 #plt.imshow(entropyImg, cmap="gray")
-fig, ax = try_all_threshold(entropyImg, figsize=(10,8), verbose = False)
-plt.show()
+edgeCanny = feature.canny(img, sigma=2)
+plt.imshow(edgeCanny, cmap="gray")
+#fig, ax = try_all_threshold(edgeCanny, figsize=(10,8), verbose = False)
+thresh =threshold_otsu(edgeCanny)
+
+binary = edgeCanny<=thresh
+plt.imshow(binary, cmap="gray")
